52787a7722932fc4b4c2e0630869f9f1eca06951
Michell Ibarra <mmibarra2@espe.edu.ec>
2026-07-07 20:02:25 -0500

    chore: inicializa proyecto spring webflux

---
+++ src/main/java/com/aerolinea/reservationevents/ReservationeventsApplication.java
@@ -0,0 +1,13 @@
+package com.aerolinea.reservationevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationeventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationeventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aerolinea/reservationevents/ReservationeventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aerolinea.reservationevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationeventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
